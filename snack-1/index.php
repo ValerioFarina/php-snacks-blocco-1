88444a5fa13c899a871ab025fee62ac4dc45d42e
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-15 14:32:55 +0100

    Creato un array matches contenente dei sotto-array che corrispondono a partite di basket

---
+++ snack-1/index.php
@@ -0,0 +1,56 @@
+<?php
+    // Creare un array $matches che contiene dei sotto-array, i quali rappresentano delle partite di basket.
+    // ogni partita avrà le seguenti caratteristiche:
+    // squadra di casa
+    // squadra ospite
+    // punti fatti dalla squadra di casa
+    // punti fatti dalla squadra ospite
+    // L'obbiettivo è stampare a schermo tutte le partite utilizzando questo schema:
+    // [squadra di casa] - [squadra ospite] | [punti squadra di casa] - [punti squadra ospite]
+    // ad esempio:
+    // Olimpia Milano - Cantù | 55 - 60
+
+
+    // we create an array $matches
+    // an element of $matches is an associative array representing a basketball match
+    // such an associative array has 2 keys:
+    // - home_team
+    // - visiting_team
+    // both home_team and visiting_team are in turn associative arrays having two keys:
+    // - name
+    // - score
+    $matches = [
+        [
+            'home_team' => [
+                'name' => 'Atlanta Hawks',
+                'score' => 75
+            ],
+            'visiting_team' => [
+                'name' => 'Los Angeles Lakers',
+                'score' => 84
+            ]
+        ],
+        [
+            'home_team' => [
+                'name' => 'Boston Celtics',
+                'score' => 81
+            ],
+            'visiting_team' => [
+                'name' => 'Miami Heat',
+                'score' => 78
+            ]
+        ],
+        [
+            'home_team' => [
+                'name' => 'New York Knicks',
+                'score' => 102
+            ],
+            'visiting_team' => [
+                'name' => 'Orlando Magic',
+                'score' => 97
+            ]
+        ]
+    ];
+
+    var_dump($matches);
+?>
