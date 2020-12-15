a08b1e6646dacdc4591a84be283dec23202c328b
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-15 15:33:39 +0100

    Creata variable "is_name_valid" che è vera se la lunghezza del nome è maggiore di 3 e falsa altrimenti

--- snack-2/index.php
+++ snack-2/index.php
@@ -13,5 +13,16 @@
 
     echo 'name: ' . $name . "<br>";
     echo 'email: ' . $email . "<br>";
-    echo 'age: ' . $age;
+    echo 'age: ' . $age . "<br>";
+
+    // we create a variable $is_name_valid, and we set it equal to the value of the expression "strlen($name) > 3"
+    // in this way, if $name contains more than 3 characters, then $is_name_valid contains the value true,
+    // otherwise $is_name_valid contains the value false
+    $is_name_valid = strlen($name) > 3;
+    if($is_name_valid) {
+        echo "The name's length is greater than 3";
+    } else {
+        echo "The name's length is not greater than 3";
+    }
+
 ?>
